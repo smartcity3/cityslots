1e7fbf60f554b08edec153153c8b1c073fe3fd7e
Mike Zoubos <zoumposmihalis@yahoo.com>
2019-05-11 19:40:43 +0300

    ...

--- api/database/Dbconnection.js
+++ api/database/Dbconnection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
-    host:'127.0.0.1',
-    user:'root',
+    host:'',
+    user:'',
     password:'',
     database:'citySlots'
 });
