3793d61a97b0aa3f51ef91d7751c53d18c92af0f
Eni Puska <eni.puska@gmail.com>
2019-05-11 16:12:33 +0300

    basic graphics of client ui

---
+++ clientUI/clientUiController.js
@@ -0,0 +1,36 @@
+app = angular.module('clientUiModule', ['ui.bootstrap']);
+
+app.directive('login', function() {
+    return {
+      templateUrl: './login/login.html'
+    };
+});
+
+app.directive('client', function() {
+    return {
+      templateUrl: './clientUI/client.html'
+    };
+});
+
+app.controller('clientUiController', ['$scope','$http', function($scope,$http) {
+    $scope.userToken = "";
+    $scope.mainClientUIVisible = true;
+    $scope.loginPageVisible = false;
+    $scope.searchDate = new Date();
+    $scope.searchTime = new Date(0, 0, 0, 0,0,0);
+    $scope.login = function() {
+        var userObject = {
+            username:$scope.username,
+            password:$scope.password
+        }
+        $http.post("http://172.16.220.205:3200/users/login/",userObject).then(function(response) {
+            if(response.data.success){
+                $scope.userToken = response.data.token;
+                $scope.loginPageVisible = false;
+                $scope.mainClientUIVisible = true;
+            }
+		},function(error){
+			$scope.loginMsg = "Failed Login!"
+		});
+    }
+}]);
